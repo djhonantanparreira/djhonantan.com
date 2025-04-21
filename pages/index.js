d41effc0d1d5d5c92dd82c213034b1ca37cb35a5
Djhonantan Parreira <djhonantanedu@gmail.com>
2025-04-20 21:30:01 -0300

    Add Prettier for code formatting and update styles for consistency

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 export default function Home() {
-    return (
+  return (
     <div className="container">
-        <div className="bars-common bar-one"></div>
-        <div className="bars-common bar-two"></div>
-        <div className="bars-common bar-three"></div>
-        <div className="squares-common square-one"></div>
-        <div className="squares-common square-two"></div>
+      <div className="bars-common bar-one"></div>
+      <div className="bars-common bar-two"></div>
+      <div className="bars-common bar-three"></div>
+      <div className="squares-common square-one"></div>
+      <div className="squares-common square-two"></div>
     </div>
-    );
+  );
 }
